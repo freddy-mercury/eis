90da9f80dfcfb78cddca7533bbe82cadecd410e5
root <root@sys-ws-09.(none)>
2012-08-17 09:40:45 +0400

    Rates.php

--- protected/components/Rates.php
+++ protected/components/Rates.php
@@ -1,20 +1,36 @@
 <?php
 class Rates
 {
+	private static $rates;
+
+	private static function calcRates() {
+		$rates_config = Yii::app()->params['rates'];
+		$date_start = $rates_config['date_start'];
+		$buy = $rates_config['buy'];
+		$sell = $rates_config['sell'];
+		$percent = $rates_config['percent'];
+		$period = $rates_config['period'];
+
+		self::$rates = array();
+		self::$rates[$date_start]['buy'] = $buy;
+		self::$rates[$date_start]['sell'] = $sell;
+		while ($date_start <= mktime(0, 0, 0, 12, 31)) {
+			$date_start+= $period;
+			self::$rates[$date_start]['buy'] = self::$rates[$date_start]['sell'];
+			self::$rates[$date_start]['sell'] = 1 + $percent / 100;
+		}
+	}
 
     public static function getCurrentRates()
     {
-        extract(Yii::app()->params['rates']);
-        while ($date_start != mktime(0, 0, 0)) {
-            $date_start+= $period;
-            $buy = $sell;
-            $sell*= 1 + $percent / 100;
-        }
-        return array('buy' => round($buy, 3), 'sell' => round($sell, 3));
+	    if (empty(self::$rates)) self::calcRates();
+        $current_rates = self::$rates[mktime(0,0,0)];
+        return array('buy' => round($current_rates['buy'], 3), 'sell' => round($current_rates['sell'], 3));
     }
 
     public static function renderTable()
     {
+	    if (empty(self::$rates)) self::calcRates();
         $html = '
         <style type="text/css">
         table.rates {
@@ -50,25 +66,14 @@ class Rates
                 <th>' . Yii::t('gloabl', 'Buy') . '</th>
                 <th>' . Yii::t('gloabl', 'Sell') . '</th>
             </tr>';
-        extract(Yii::app()->params['rates']);
-        while ($date_start <= mktime(0, 0, 0, 12, 31)) {
-            $style = $target = '';
-            if((mktime(0, 0, 0) - $date_start) / $period == 10) {
-                $target = '<a name="1"></a>';
-            }
-            elseif (mktime(0, 0, 0) == $date_start) {
-                $style = ' style="color: red"';
-            }
-            $html .= '
-            <tr'.$style.'>
-                <th>'.$target.date('Y-m-d', $date_start).'</th>
-                <td>'.number_format($buy, 3).'</td>
-                <td>'.number_format($sell, 3).'</td>
+	    foreach (self::$rates as $date_start => $rates) {
+		    $html .= '
+            <tr>
+                <th>'.date('Y-m-d', $date_start).'</th>
+                <td>'.number_format($rates['buy'], 3).'</td>
+                <td>'.number_format($rates['sell'], 3).'</td>
             </tr>';
-            $date_start+= $period;
-            $buy = $sell;
-            $sell*= 1 + $percent / 100;
-        }
+	    }
         $html.= '</table>';
         return $html;
     }
