3764d4551adeee797115614240972ab09a9b75e8
Freddy Mercury <kirill.komarov@gmail.com>
2012-08-17 11:48:18 +0400

    ik

--- protected/controllers/InterkassaController.php
+++ protected/controllers/InterkassaController.php
@@ -5,7 +5,12 @@ class InterkassaController extends Controller
 	public function actionIndex()
 	{
         if (Yii::app()->interkassa->chechPayment($_REQUEST)) {
-            // @todo заносим платеж в БД
+	        /* @var $transaction RatesTransaction */
+            $transaction = RatesTransaction::model()->findByPk($_REQUEST['ik_payment_id']);
+	        if ($transaction !== null) {
+		        $transaction->status = 1;
+		        $transaction->save();
+	        }
         }
 	}
 
